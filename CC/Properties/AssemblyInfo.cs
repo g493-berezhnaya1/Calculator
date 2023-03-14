bbc63a61b371b50f80dc1f2894aa6b7608207509
g493-berezhnaya1 <126170641+g493-berezhnaya1@users.noreply.github.com>
2023-03-14 13:26:53 +0300

    Add files via upload

---
+++ CC/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CC")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3105775f-95d7-467f-99ec-5a47db18ce0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CC/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System;
+using WSUnivercalLib;
+
+namespace CC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+                int pType = 3;
+                int mType = 1;
+                int count = 15;
+                float width = 20;
+                float length = 45;
+                int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+                Assert.AreEqual(setRes, -1);
+           }
+            [TestMethod]
+            public void invalid_material()
+            {
+                int pType = 3;
+                int mType = 1;
+                int count = 15;
+                float width = 20;
+                float length = 45;
+                int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+                Assert.AreEqual(setRes, -1);
+            }
+            [TestMethod]
+            public void count_lower_zero()
+            {
+                int pType = 3;
+                int mType = 5;
+                int count = -3;
+                float width = 20;
+                float length = 45;
+                int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+                Assert.AreEqual(setRes, -1);
+            }
+
+            [TestMethod]
+            public void calculate_heigth_width_value()
+            {
+                int pType = 3;
+                int mType = 1;
+                int count = 15;
+                float width = 20;
+                float length = 45;
+                int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+                Assert.AreEqual(setRes, -1);
+            }
+
+            [TestMethod]
+            public void Product_isnot_performed()
+         {
+            int pType = 3;
+            int mType = 5;
+            int count = 15;
+            float width = 0;
+            float length = 45;
+            int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+            Assert.AreEqual(setRes, -1);
+        }
+        [TestMethod]
+        public void Performed_heigth_is_0_then_calc_is_not_performed()
+        {
+            int pType = 3;
+            int mType = 5;
+            int count = 15;
+            float width = 20;
+            float length = 0;
+            int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+            Assert.AreEqual(setRes, -1);
+        }
+
+        [TestMethod]
+        public void Calc_width_int_and_height_value()
+        {
+            int pType = 2;
+            int mType = 2;
+            int count = 5;
+            float width = 10;
+            float length = 10;
+            int setRes = Calculation.GetQuantityForProduct(pType, mType, count, width, length);
+            Assert.AreEqual(setRes, -1);
+        }
+
+    }
+}
+
